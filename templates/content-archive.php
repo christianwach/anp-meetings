1478811fc646d2f416e82b6ff80b9cf44a85faeb
Pea <pea@misfist.com>
2017-01-31 20:04:14 -0500

    Cleaned Archive Content Template
    Cleaned up markup in archive content template.

--- templates/content-archive.php
+++ templates/content-archive.php
@@ -4,86 +4,90 @@
  * Content Variables - DO NOT REMOVE
  * Variables that can be used in the template
  */
-
-global $wp_query;
-
-$post_type = get_post_type( get_the_ID() );
+$post_id = $post->ID;
+$post_type = get_post_type( $post_id );
 
 // Meeting Meta
-$meeting_date = ( get_post_meta( get_the_ID(), 'meeting_date', true ) ) ? date_i18n( get_option( 'date_format' ), strtotime( get_post_meta( get_the_ID(), 'meeting_date', true ) ) ) : '' ;
-$organization = get_the_term_list( get_the_ID(), 'organization', '<span class="organization tag">', ', ', '</span>' );
-$meeting_type = get_the_term_list( get_the_ID(), 'meeting_type', '<span class="meeting-type tag">', ', ', '</span>' );
-$meeting_tags = get_the_term_list( get_the_ID(), 'meeting_tag', '<span class="meeting-tag tag">', ', ', '</span>' );
+$meeting_date = ( get_post_meta( $post_id, 'meeting_date', true ) ) ? date_i18n( get_option( 'date_format' ), strtotime( get_post_meta( $post_id, 'meeting_date', true ) ) ) : '' ;
+$organization = get_the_term_list( $post_id, 'organization', '<span class="organization term">', ', ', '</span>' );
+$meeting_type = get_the_term_list( $post_id, 'meeting_type', '<span class="meeting-type term">', ', ', '</span>' );
+$meeting_tags = get_the_term_list( $post_id, 'meeting_tag', '<span class="meeting-tag term">', ', ', '</span>' );
 
 // Proposal Meta
 $approval_date = $meeting_date;
-$effective_date = get_post_meta( get_the_ID(), 'proposal_date_effective', true );
-$proposal_status = get_the_term_list( get_the_ID(), 'proposal_status', '<span class="proposal-status tag">', ', ', '</span>' ); ?>
+$effective_date = get_post_meta( $post_id, 'proposal_date_effective', true );
+$proposal_status = get_the_term_list( $post_id, 'proposal_status', '<span class="proposal-status term">', ', ', '</span>' ); ?>
 
-<div class="meta meeting-meta"><?php echo ( !empty( $meeting_date ) ) ? '<span class="meta-label">' . __( 'Date:','anp-meetings'  ) . '</span> ' . $meeting_date : ''; ?></div>
-<div class="meta meeting-meta"><?php echo ( !empty( $organization ) ) ? '<span class="meta-label">' . __( 'Organization:', 'anp-meetings' ) . '</span> ' . $organization : ''; ?></div>
-<div class="meta meeting-meta"><?php echo ( !empty( $meeting_tags ) ) ? '<span class="meta-label">' . __( 'Tags:', 'anp-meetings'  ) . '</span> ' . $meeting_tags : '' ; ?></div>
+<?php if( !empty( $meeting_date ) ) : ?>
+  <div class="meta meeting-meta">
+    <span class="meta-label"><?php _e( 'Date:','meetings'  ); ?></span> <?php echo $meeting_date; ?>
+  </div>
+<?php endif; ?>
+
+<?php if( !empty(  $organization ) ) : ?>
+  <div class="meta meeting-meta">
+    <span class="meta-label"><?php _e( 'Organization:', 'meetings' ); ?></span> <?php echo $organization; ?>
+  </div>
+<?php endif; ?>
+
+<?php if( !empty(  $meeting_tags ) ) : ?>
+  <div class="meta meeting-meta">
+    <span class="meta-label"><?php _e( 'Tags:', 'meetings'  ); ?></span> <?php echo $meeting_tags; ?>
+  </div>
+<?php endif; ?>
 
 <?php
-if( 'meeting' == $post_type ) { ?>
+if( 'meeting' == $post_type ) : ?>
 
     <div class="meta meeting-meta"><?php echo ( !empty( $meeting_type ) ) ? __( 'Type: ', 'anp-meetings'  ) . $meeting_type : '' ; ?></div>
 
     <?php
-    $agendas = ( function_exists( 'meeting_get_agenda' ) ) ? meeting_get_agenda( get_the_ID() ) : '';
+    $agendas = ( function_exists( 'meeting_get_agenda' ) ) ? meeting_get_agenda( $post_id ) : '';
+    $summaries = ( function_exists( 'meeting_get_summary' ) ) ? meeting_get_summary( $post_id ) : '';
+    $proposals = ( function_exists( 'meeting_get_proposal' ) ) ? meeting_get_proposal( $post_id ) : '';
 
-    $summaries = ( function_exists( 'meeting_get_summary' ) ) ? meeting_get_summary( get_the_ID() ) : '';
-
-    $proposals = ( function_exists( 'meeting_get_proposal' ) ) ? meeting_get_proposal( get_the_ID() ) : '';
-
-    if( $agendas || $summaries || $proposals ) { ?>
+    if( $agendas || $summaries || $proposals ) : ?>
 
         <ul class="connected-content">
 
-        <?php echo ( $agendas ) ? $agendas : ''; ?>
-
-        <?php echo ( $summaries ) ? $summaries : ''; ?>
-
-        <?php echo ( $proposals ) ? $proposals : ''; ?>
+          <?php echo ( $agendas ) ? $agendas : ''; ?>
+          <?php echo ( $summaries ) ? $summaries : ''; ?>
+          <?php echo ( $proposals ) ? $proposals : ''; ?>
 
         </ul>
 
-    <?php
-    }
-}
+    <?php endif; ?>
 
-if( 'agenda' == $post_type ) {
+<?php endif; ?>
 
-    $agendas = ( function_exists( 'meeting_get_agenda' ) ) ? meeting_get_agenda( get_the_ID() ) : '';
+<?php if( 'agenda' == $post_type ) : ?>
 
-    if( $agendas ) { ?>
+    <?php $agendas = ( function_exists( 'meeting_get_agenda' ) ) ? meeting_get_agenda( $post_id ) : ''; ?>
+
+    <?php if( $agendas ) : ?>
 
         <ul class="connected-content meeting">
 
         <?php ( $agendas ) ? $agendas : ''; ?>
 
-        </ul>'
+        </ul>
 
-    <?php
-    }
+    <?php endif; ?>
 
-}
+<?php endif; ?>
 
-if( 'summary' == $post_type ) {
+<?php if( 'summary' == $post_type ) :  ?>
 
-    $summaries = ( function_exists( 'meeting_get_summary' ) ) ? meeting_get_summary( get_the_ID() ) : '';
+    <?php $summaries = ( function_exists( 'meeting_get_summary' ) ) ? meeting_get_summary( $post_id ) : ''; ?>
 
-    if( $summaries ) { ?>
+    <?php if( !empty( $summaries ) ) :  ?>
 
         <ul class="connected-content meeting">
 
-        <?php echo ( $summaries ) ? $summaries : ''; ?>
+          <?php echo $summaries; ?>
 
         </ul>
 
-    <?php
-    }
-}
+    <?php endif; ?>
 
-
-?>
+<?php endif; ?>
