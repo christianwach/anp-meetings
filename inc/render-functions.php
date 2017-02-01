60bb4df15e9a4e08f003120c27780b70cc714445
Pea <pea@misfist.com>
2017-01-31 21:18:46 -0500

    Refactured Functions

--- inc/render-functions.php
+++ inc/render-functions.php
@@ -1,6 +1,6 @@
 <?php
 
-/* ADMIN CONNECTION 
+/* ADMIN CONNECTION
  * Order posts alphabetically in the P2P connections box
  *
  */
@@ -20,11 +20,6 @@ if(! function_exists( 'anp_connection_box_order' ) ) {
 
 }
 
-
-/**
- * CONNECTION RENDERING
- */
-
 /**
  * Agenda
  * Render agenda associated with content
@@ -33,34 +28,32 @@ if(! function_exists( 'anp_connection_box_order' ) ) {
 if(! function_exists( 'meeting_get_agenda' ) ) {
 
     function meeting_get_agenda( $post_id ) {
-     
+
         $query_args = array(
             'connected_type' => 'meeting_to_agenda',
-            'connected_items' => intval( $post_id ),        
+            'connected_items' => intval( $post_id ),
             'nopaging' => true
         );
-         
+
         $agendas = get_posts( $query_args );
-        
-        $content = '';
 
-        if( count( $agendas ) > 0 ) {
-
-            foreach( $agendas as $post ) {
-
-                $post_type_obj = get_post_type_object( get_post_type( $post->ID ) );
-                $post_type_name = ( $post_type_obj ) ? $post_type_obj->labels->singular_name : '';
-
-                $content .= '<li class="agenda-link"><a href="' . get_post_permalink( $post->ID ) . '">';
-                $content .= ( $post_type_name ) ? $post_type_name : $post->post_title;
-                $content .= '</a></li>';
-            }         
-
+        if( empty( $agendas ) ) {
+            return;
         }
+
+        $post = $agendas[0];
+
+        $post_type_obj = get_post_type_object( get_post_type( $post->ID ) );
+        $post_type_name = ( $post_type_obj ) ? $post_type_obj->labels->singular_name : '';
+
+        $content = sprintf( '<li class="agenda-link"><a href="%s" rel="bookmark"><span class="link-text">%s</span></a></li>',
+          get_post_permalink( $post->ID ),
+          ( $post_type_name ) ? $post_type_name : $post->post_title
+        );
 
         // Filter added to allow content be overriden
         return apply_filters( 'meeting_get_agenda_content', $content, $post_id );
-    }  
+    }
 
 }
 
@@ -68,81 +61,117 @@ if(! function_exists( 'meeting_get_agenda' ) ) {
  * Summary
  * Render summary associated with content
  */
-
 if(! function_exists( 'meeting_get_summary' ) ) {
 
     function meeting_get_summary( $post_id ) {
-     
+
         $query_args = array(
             'connected_type' => 'meeting_to_summary',
-            'connected_items' => intval( $post_id ),        
+            'connected_items' => intval( $post_id ),
             'nopaging' => true
         );
-         
+
         $summaries = get_posts( $query_args );
-        
-        $content = '';
 
-        if( count( $summaries ) > 0 ) {
-
-            foreach( $summaries as $post ) {
-
-                $post_type_obj = get_post_type_object( get_post_type( $post->ID ) );
-                $post_type_name = ( $post_type_obj ) ? $post_type_obj->labels->singular_name : '';
-
-                $content .= '<li class="summary-link"><a href="' . get_post_permalink( $post->ID ) . '">';
-                $content .= ( $post_type_name ) ? $post_type_name : $post->post_title;
-                $content .= '</a></li>';
-            }         
-
+        if( empty( $summaries ) ) {
+          return;
         }
+
+        $post = $summaries[0];
+
+        $post_type_obj = get_post_type_object( get_post_type( $post->ID ) );
+        $post_type_name = ( $post_type_obj ) ? $post_type_obj->labels->singular_name : '';
+
+        $content = sprintf( '<li class="summary-link"><a href="%s" rel="bookmark"><span class="link-text">%s</span></a></li>',
+          get_post_permalink( $post->ID ),
+          ( $post_type_name ) ? $post_type_name : $post->post_title
+        );
 
         // Filter added to allow content be overriden
         return apply_filters( 'meeting_get_summary_content', $content, $post_id );
-    }  
-      
+    }
+
 }
 
 /**
  * Proposal
  * Render proposal associated with content
  */
-
 if(! function_exists( 'meeting_get_proposal' ) ) {
 
     function meeting_get_proposal( $post_id ) {
-     
+
         $query_args = array(
             'connected_type' => 'meeting_to_proposal',
-            'connected_items' => intval( $post_id ),        
+            'connected_items' => intval( $post_id ),
             'nopaging' => true
         );
-         
+
         $proposals = get_posts( $query_args );
 
-        $url = array( 
-            'connected_type' => 'meeting_to_proposal', 
+        if( empty( $proposals ) ) {
+          return;
+        }
+
+        $url = array(
+            'connected_type' => 'meeting_to_proposal',
             'connected_items' => intval( $post_id ),
             'connected_direction' => 'from'
         );
-        
-        $content = '';
 
-        if( count( $proposals ) > 0 ) {
-
-            $content .= '<li class="proposal-link"><a href="' . esc_url( add_query_arg( $url ) ) . '">';
-            $content .= ( 1 == count( $proposals ) ) ? __( 'Proposal', 'meeting' ) : __( 'Proposals', 'meeting' );
-            $content .= '</a></li>';                   
-
-        }
+        $content = sprintf( '<li class="proposal-link"><a href="%s" rel="bookmark"><span class="link-text">%s</span></a></li>',
+          esc_url( add_query_arg( $url ) ),
+          ( 1 == count( $proposals ) ) ? __( 'Proposal', 'meeting' ) : __( 'Proposals', 'meeting' )
+        );
 
         // Filter added to allow content be overriden
         return apply_filters( 'meeting_get_proposal_content', $content, $post_id );
-    }  
-      
+    }
+
 }
 
-/* 
+/**
+ * Get Events
+ * Return event connected to post
+ *
+ * @since 1.0.0
+ *
+ * @param  int $post_id
+ * @return string event
+ */
+ if(! function_exists( 'meeting_get_event' ) ) {
+
+     function meeting_get_event( $post_id ) {
+
+         $query_args = array(
+             'connected_type' => 'meeting_to_event',
+             'connected_items' => intval( $post_id ),
+             'nopaging' => true
+         );
+
+         $events = get_posts( $query_args );
+
+         if( empty( $events ) ) {
+           return;
+         }
+
+         $post = $events[0];
+
+         $post_type_obj = get_post_type_object( get_post_type( $post->ID ) );
+         $post_type_name = ( $post_type_obj ) ? $post_type_obj->labels->singular_name : '';
+
+         $content = sprintf( '<li class="event-link"><a href="%s" rel="bookmark"><span class="link-text">%s</span></a></li>',
+           get_post_permalink( $post->ID ),
+           ( $post_type_name ) ? $post_type_name : $post->post_title
+         );
+
+         // Filter added to allow content be overriden
+         return apply_filters( 'meeting_get_event_content', $content, $post_id );
+     }
+
+ }
+
+/*
  * Add markdown support for custom post types
  */
 
@@ -160,21 +189,19 @@ if(! function_exists( 'meeting_markdown_support' )  ) {
 }
 
 
-/* 
+/*
  * TEMPLATE LOCATION
- * Templates can be overwritten by putting a template file of the same name in 
- * plugins/anp-meeting/ folder of your active theme 
+ * Templates can be overwritten by putting a template file of the same name in
+ * plugins/anp-meeting/ folder of your active theme
  */
-
-
 if(! function_exists( 'include_meeting_templates' ) ) {
 
     function include_meeting_templates( $template_path ) {
 
         $post_types = array(
-            'meeting', 
-            'proposal', 
-            'summary', 
+            'meeting',
+            'proposal',
+            'summary',
             'agenda'
         );
 
