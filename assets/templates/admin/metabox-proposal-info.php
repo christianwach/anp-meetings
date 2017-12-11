14bab0c03a4138332916aca5ec4f0a804b908e42
Christian Wach <needle@haystack.co.uk>
2017-12-11 13:30:43 +0000

    reduce whitespace in Proposals metabox

--- assets/templates/admin/metabox-proposal-info.php
+++ assets/templates/admin/metabox-proposal-info.php
@@ -1,9 +1,9 @@
 <!-- assets/templates/admin/metabox-meeting-info.php -->
 <?php wp_nonce_field( 'wordpress_meetings_proposal_info_box', 'wordpress_meetings_proposal_info_nonce' ); ?>
 
-<p><strong><label for="<?php echo $this->status_meta_key; ?>"><?php _e( 'Status', 'wordpress-meetings' ); ?></label></strong></p>
+<p><strong><label for="<?php echo $this->status_meta_key; ?>"><?php _e( 'Status', 'wordpress-meetings' ); ?></label></strong><br />
 
-<p><?php wp_dropdown_categories( array(
+<?php wp_dropdown_categories( array(
 	'taxonomy' => 'proposal_status',
 	'show_option_none' => __( 'No status', 'wordpress-meetings' ),
 	'id' => $this->status_meta_key,
@@ -13,11 +13,11 @@
 	'selected' => $status,
 ) ); ?></p>
 
-<p><strong><label for="<?php echo $this->date_accepted_meta_key; ?>"><?php _e( 'Date Accepted', 'wordpress-meetings' ); ?></label></strong></p>
+<p><strong><label for="<?php echo $this->date_accepted_meta_key; ?>"><?php _e( 'Date Accepted', 'wordpress-meetings' ); ?></label></strong><br />
 
-<p><input type="text" id="<?php echo $this->date_accepted_meta_key; ?>" name="<?php echo $this->date_accepted_meta_key; ?>" class="wp_datepicker" value="<?php echo $date_accepted; ?>" /></p>
+<input type="text" id="<?php echo $this->date_accepted_meta_key; ?>" name="<?php echo $this->date_accepted_meta_key; ?>" class="wp_datepicker" value="<?php echo $date_accepted; ?>" /></p>
 
-<p><strong><label for="<?php echo $this->date_effective_meta_key; ?>"><?php _e( 'Date Effective', 'wordpress-meetings' ); ?></label></strong></p>
+<p><strong><label for="<?php echo $this->date_effective_meta_key; ?>"><?php _e( 'Date Effective', 'wordpress-meetings' ); ?></label></strong><br />
 
-<p><input type="text" id="<?php echo $this->date_effective_meta_key; ?>" name="<?php echo $this->date_effective_meta_key; ?>" class="wp_datepicker" value="<?php echo $date_effective; ?>" /></p>
+<input type="text" id="<?php echo $this->date_effective_meta_key; ?>" name="<?php echo $this->date_effective_meta_key; ?>" class="wp_datepicker" value="<?php echo $date_effective; ?>" /></p>
 
